57f6c549f5d923808d483d497d0b832e3d02a762
choyongbu <choyongbu7@gmail.com>
2021-03-22 20:30:10 +0900

    부트스트랩 클린  블로그 테마 적용

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def contact(request):
+    return render(request, 'contact.html')
